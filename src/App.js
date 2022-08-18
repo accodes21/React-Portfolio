bce7b7b8690160ef57977685330e7e485712fbb3
Aarya Chopkar <aaryachopkar@gmail.com>
2022-08-18 16:06:02 +0530

    Initial styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+import './App.scss';
+import {Routes, Route} from 'react-router-dom';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
-    <></>
+    <>
+    <Routes>
+      <Route path="/" element={<Layout/>}/>
+    </Routes>
+    </>
   );
 }
 
